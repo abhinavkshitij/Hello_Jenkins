546af419c25dc121d32f9789b0d1ddb2423332cf
Abhinav <C-Abhinav.Kshitij@charter.com>
2021-07-16 14:57:37 -0600

    note on grok update

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Jenkins Hello World example\n";
   cout << "Testing webhook with ngrok after ssh-auth\n";
   cout << "Added correct webhook payload with ngrok\n";
+  cout << "Public IP exposed by ngrok is dynamic, requires updating payload address";
   return 0;
 }
